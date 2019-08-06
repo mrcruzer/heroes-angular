cd03811d9b5743adfdb437282b40562176c62516
Isaac Avelino <isaacavelino14@gmail.com>
2019-08-06 17:04:15 -0400

    Initial commit

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../../services/heroes.services';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styles: []
+})
+export class HeroesComponent implements OnInit {
+
+  heroes:Heroe[] = [];
+
+  constructor(private _heroesService: HeroesService) {
+
+    console.log("constructor");
+    
+   }
+
+  ngOnInit() {
+
+    this.heroes = this._heroesService.getHeroes();
+
+    console.log(this.heroes);
+
+  }
+
+}
